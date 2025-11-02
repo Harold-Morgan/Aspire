dfa69a266a20027b815d2724dd886cefff9205b2
hmorgan <volik46@gmail.com>
2025-11-02 23:17:47 +0300

    clickhouse integration draft (wip)

---
+++ src/CommunityToolkit.Aspire.Hosting.ClickHouse/ClickHouseBuilderExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+namespace CommunityToolkit.Aspire.Hosting.ClickHouse;
+
+/// <summary>
+/// Provides extension methods for adding ClickHouse resources to an <see cref="IDistributedApplicationBuilder"/>.
+/// </summary>
+public static class ClickHouseBuilderExtensions
+{
+    private const int ClickHouseDefaultPort = 8123;
+    private const string UserEnvVarName = "CLICKHOUSE_USER";
+    private const string PasswordEnvVarName = "CLICKHOUSE_PASS";
+
+    /// <summary>
+    /// Adds a ClickHouse resource to the application model. A container is used for local development.
+    /// The default image is <inheritdoc cref="ClickHouseContainerImageTags.Image"/> and the tag is <inheritdoc cref="ClickHouseContainerImageTags.Tag"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IDistributedApplicationBuilder"/>.</param>
+    /// <param name="name">The name of the resource. This name will be used as the connection string name when referenced in a dependency.</param>
+    /// <param name="userName">The parameter used to provide the administrator username for the ClickHouse resource.</param>
+    /// <param name="password">The parameter used to provide the administrator password for the ClickHouse resource. If <see langword="null"/> a random password will be generated.</param>
+    /// <param name="port">The host port for the ClickHouse instance.</param>
+    /// <returns>A reference to the <see cref="IResourceBuilder{T}"/>.</returns>
+    /// <remarks>
+    /// <example>
+    /// Add a ClickHouse container to the application model and reference it in a .NET project.
+    /// <code lang="csharp">
+    /// var builder = DistributedApplication.CreateBuilder(args);
+    ///
+    /// var db = builder.AddClickHouseServer("clickhouse")
+    ///   .AddDatabase("db");
+    /// var api = builder.AddProject&lt;Projects.Api&gt;("api")
+    ///   .WithReference(db);
+    ///  
+    /// builder.Build().Run(); 
+    /// </code>
+    /// </example>
+    /// </remarks>
+    public static IResourceBuilder<ClickHouseServerResource> AddClickHouseServer(
+        this IDistributedApplicationBuilder builder,
+        [ResourceName] string name,
+        IResourceBuilder<ParameterResource>? userName = null,
+        IResourceBuilder<ParameterResource>? password = null,
+        int? port = null
+    )
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        
+        // The password must be at least 8 characters long and contain characters from three of the following four sets: Uppercase letters, Lowercase letters, Base 10 digits, and Symbols
+        var passwordParameter = password?.Resource ?? ParameterResourceBuilderExtensions.CreateDefaultPasswordParameter(builder, $"{name}-password", minLower: 1, minUpper: 1, minNumeric: 1);
+        
+        var clickHouseServer = new ClickHouseServerResource(name, userName?.Resource, passwordParameter);
+
+        return builder.AddResource(clickHouseServer)
+                      .WithEndpoint(port: port, targetPort: ClickHouseDefaultPort, name: ClickHouseServerResource.PrimaryEndpointName)
+                      .WithImage(ClickHouseContainerImageTags.Image, ClickHouseContainerImageTags.Tag)
+                      .WithImageRegistry(ClickHouseContainerImageTags.Registry)
+                      .WithEnvironment(context =>
+                      {
+                          context.EnvironmentVariables[UserEnvVarName] = clickHouseServer.UserNameReference;
+                          context.EnvironmentVariables[PasswordEnvVarName] = clickHouseServer.Password;
+                      })
+                      .OnResourceReady(async (_, @event, ct) =>
+                      {
+
+                          var connectionString = await clickHouseServer.GetConnectionStringAsync(ct).ConfigureAwait(false);
+                          if (connectionString is null)
+                          {
+                              throw new DistributedApplicationException($"ResourceReadyEvent was published for the '{clickHouseServer.Name}' resource but the connection string was null.");
+                          }
+                      });
+    }
+}
---
+++ src/CommunityToolkit.Aspire.Hosting.ClickHouse/ClickHouseServerResource.cs
@@ -0,0 +1,83 @@
+﻿using Aspire.Hosting.ApplicationModel;
+
+namespace CommunityToolkit.Aspire.Hosting.ClickHouse;
+
+/// <summary>
+/// A resource that represents a ClickHouse db server
+/// </summary>
+/// <param name="name">The name of the resource.</param>
+/// <param name="userName">A parameter that contains the ClickHouse username.</param>
+/// <param name="password">A parameter that contains the ClickHouse password.</param>
+public sealed class ClickHouseServerResource(
+    [ResourceName] string name,
+    ParameterResource? userName,
+    ParameterResource password
+    ) : ContainerResource(name), IResourceWithConnectionString
+{
+    internal const string PrimaryEndpointName = "http";
+    internal const string DefaultUserName = "default";
+
+    /// <summary>
+    /// The ClickHouse default user.
+    /// </summary>
+    public ParameterResource? UserName { get; set; } = userName;
+    
+    internal ReferenceExpression UserNameReference =>
+        UserName is not null ?
+            ReferenceExpression.Create($"{UserName}") :
+            ReferenceExpression.Create($"{DefaultUserName}");
+
+    /// <summary>
+    /// The ClickHouse root password.
+    /// </summary>
+    public ParameterResource Password { get; private set; } = password;
+
+    private EndpointReference? _primaryEndpoint;
+
+    /// <summary>
+    /// Gets the primary endpoint for the ClickHouse server instance.
+    /// </summary>
+    public EndpointReference PrimaryEndpoint => _primaryEndpoint ??= new(this, PrimaryEndpointName);
+    
+    /// <summary>
+    /// Gets the connection string expression for the ClickHouse
+    /// </summary>
+    public ReferenceExpression ConnectionStringExpression => GetConnectionString();
+    
+    /// <summary>
+    /// Gets the connection string for the ClickHouse server.
+    /// </summary>
+    /// <param name="cancellationToken"> A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <returns>A connection string for the ClickHouse server in the form "Server=scheme://host:port;User=username;Password=password"</returns>
+    public ValueTask<string?> GetConnectionStringAsync(CancellationToken cancellationToken = default)
+    {
+        if (this.TryGetLastAnnotation<ConnectionStringRedirectAnnotation>(out var connectionStringAnnotation))
+        {
+            return connectionStringAnnotation.Resource.GetConnectionStringAsync(cancellationToken);
+        }
+
+        return ConnectionStringExpression.GetValueAsync(cancellationToken);
+    }
+    
+    /// <summary>
+    /// Gets the connection string for the ClickHouse server.
+    /// </summary>
+    private ReferenceExpression GetConnectionString()
+    {
+        var builder = new ReferenceExpressionBuilder();
+
+        builder.Append(
+            $"Host={PrimaryEndpoint.Property(EndpointProperty.Host)}:{PrimaryEndpoint.Property(EndpointProperty.Port)}");
+
+        builder.Append($";Protocol=http");
+        builder.Append($";Username={UserNameReference}");
+        builder.Append($";Password={Password}");
+
+        return builder.Build();
+    }
+
+    internal void SetPassword(ParameterResource password)
+    {
+        Password = password;
+    }
+}
